89df702e236e0566371648828259238fe7540f52
Alanoxer <Johnsito616@gmail.com>
2024-02-02 13:40:57 -0300

    #

--- src/app/components/DashBoard.jsx
+++ src/app/components/DashBoard.jsx
@@ -3,7 +3,8 @@ import { useParams } from "next/navigation"
 
 const DashBoard = ()=>{
 const params = useParams()
-    if(params.keyword || params.pagination)return (<>
+
+    if(params.keyword || params.pagination) return (<>
     <aside className="flex w-72 flex-col space-y-2 border-r-2 border-red-500 bg-black p-2 pt-20" style={{height: 90.5}}
             x-show="asideOpen">
             <Link href="/news/Deportes/1" className="flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-red-600">
@@ -47,6 +48,7 @@ const params = useParams()
             </Link>
         </aside>
     </>)
+    
     else return null
 }
 
