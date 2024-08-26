6c223de772209b4486da40c3f35389febab6a7f4
Alanoxer <Johnsito616@gmail.com>
2024-08-26 00:15:45 -0300

    #

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -6,7 +6,7 @@ import { CalendarDays, MapPin, Link as LinkIcon } from "lucide-react"
 export default function ProfilePage(){
     const {data: session, status} = useSession()
 
-    console.log(session.user, status)
+    console.log(session, status)
 
     return (
             <div className="container mx-auto px-4 py-8">
